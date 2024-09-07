702ee3bf7f57f955c4afd6f9507f53f968ab267d
RayLight-369 <abdulrafay.designs@gmail.com>
2024-09-07 12:24:52 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/dashboard";
+import IsAuthenticated from "./Contexts/IsAuthenticated";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ChildLayout />
+    </BrowserRouter>
+  );
+};
+
+const ChildLayout = () => {
+  return (
+    <IsAuthenticated>
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route path="/" index element={<Home />} />
           <Route path="dashboard" index element={<Dashboard />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </IsAuthenticated>
   );
 };
 
